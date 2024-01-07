de769274603cbb50d576003b113c86dbee61828e
Warren Duan <wduan@uaschools.org>
2024-01-06 19:43:04 -0500

    bob

--- src/main/java/org/wduan/connectionsbackend/ConnectionsBackendApplication.java
+++ src/main/java/org/wduan/connectionsbackend/ConnectionsBackendApplication.java
@@ -47,8 +47,8 @@ public class ConnectionsBackendApplication {
         if (delay < 0) {
             calendar.add(Calendar.DAY_OF_MONTH, 1);
             //add 18 million ms to compensate for GMT delay
-            delay = calendar.getTimeInMillis() - System.currentTimeMillis();
-                    //+ 18000000;
+            delay = calendar.getTimeInMillis() - System.currentTimeMillis()
+                    + 18000000;
         }
         new Timer().scheduleAtFixedRate(timerTask, delay, TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
     }
